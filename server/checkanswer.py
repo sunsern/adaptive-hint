fad0a8a1ac4247114275e5c674f90d2a7da5ea07
sunsern@gmail.com <scheaman@webwork.cse.ucsd.edu>
2013-09-05 18:07:52 -0700

    Connect pgwrapper to rest_server

---
+++ server/checkanswer.py
@@ -0,0 +1,44 @@
+import tornado.ioloop
+import tornado.web
+import tornado.gen
+import logging
+import pgwrapper
+import os
+
+def task_checkanswer(pg_file, answers, seed, callback=None):
+    callback(pgwrapper.checkanswer(pg_file, answers, int(seed)))
+ 
+class CheckAnswer(tornado.web.RequestHandler):
+    """Check answers with PG"""
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    def post(self):
+        """POST /checkanswer"""
+        pg_file = self.get_argument("pg_file", default='', strip=False)
+        seed = self.get_argument("seed", default='1', strip=False)
+
+        # get AnSwEr*
+        answers = {}
+        for key in self.request.arguments:
+            if key.startswith('AnSwEr'):
+                answers[key] = self.request.arguments[key][0]
+        
+        if not os.path.isfile(pg_file):
+            self.set_status(500)
+            self.write("PG file not found\n")
+            return
+        
+        if len(answers) == 0:
+            self.set_status(500)
+            self.write("No answers given\n")
+            return
+            
+        results = yield tornado.gen.Task(task_checkanswer,
+                                         pg_file, answers, int(seed))
+        
+        if results is None:
+            self.set_status(500)
+            self.write("Something went wrong\n")
+            return
+        
+        self.write(results)
---
+++ server/render.py
@@ -0,0 +1,33 @@
+import tornado.ioloop
+import tornado.web
+import tornado.gen
+import logging
+import pgwrapper
+import os
+
+def task_render(pg_file, seed, callback=None):
+    callback(pgwrapper.render_pg(pg_file, int(seed)))
+ 
+class Render(tornado.web.RequestHandler):
+    """Render PG"""
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    def post(self):
+        """POST /render"""
+        pg_file = self.get_argument("pg_file", default='', strip=False)
+        seed = self.get_argument("seed", default='1', strip=False)
+
+        if not os.path.isfile(pg_file):
+            self.set_status(500)
+            self.write("PG file not found\n")
+            return
+        
+        rendered_html = yield tornado.gen.Task(task_render, pg_file, int(seed))
+        
+        if rendered_html is None:
+            self.set_status(500)
+            self.write("Something went wrong\n")
+            return
+        
+        response = { 'rendered_html': rendered_html }
+        self.write(response)
