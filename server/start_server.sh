429d9baa1e8aa51a8b90f1de28186ef724a0f064
sunsern@gmail.com <scheaman@webwork.cse.ucsd.edu>
2013-09-06 13:58:21 -0700

    Update start/stop scripts

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zdaemon -p "python rest_server/rest_server.py" -d -s zdsock-1 start
-zdaemon -p "python sockjs_server/sockjs_server.py" -d -s zdsock-2 start
+zdaemon -p "python rest_server/rest_server.py" -d -s zdsock-rest start
+zdaemon -p "python sockjs_server/sockjs_server.py" -d -s zdsock-sockjs start
